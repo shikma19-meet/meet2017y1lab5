6870bc75e7a7e3b7ac6ceebd7fec3dde07c88de6
shikma19-meet <shikma19@meet.mit.edu>
2017-07-30 18:28:06 +0300

    Stopping here

---
+++ stringfun.py
@@ -0,0 +1,11 @@
+def string_test(s):
+    if len(s)>2  and s[0] == s[-1]: 
+        return ('True')
+    else:
+        print (False)
+        
+   
+def add_x(s):
+    s = 'x' + s + 'x' 
+    return s
+ 
