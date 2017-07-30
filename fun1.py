d9d96837760fbef2ff2fcb02ced2dfb5399fff86
shikma19-meet <shikma19@meet.mit.edu>
2017-07-30 12:55:16 +0200

    Stopping here

--- fun1.py
+++ fun1.py
@@ -2,7 +2,17 @@
 def add_numbers (start,end):
     c = 0
     for number in range (start, end+1):
-        print (number)
+        
         c  = c + number
     return (c)
+
+test1 = add_numbers (1,2)
+print( test1)
+test2 = add_numbers(1,100)
+print(test2)
+test3 = add_numbers (1000,5000)
+print(test3)
+
+print(add_numbers(333,777))
+
 
---
+++ fun2.py
@@ -0,0 +1,8 @@
+def draw_1d(n,z):
+    a =  z**n
+    print (a)
+
+    
+
+
+
